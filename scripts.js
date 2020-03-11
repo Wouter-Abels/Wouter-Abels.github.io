c3201347a31cdc0af6ba1358523043e84972dfa8
Wouter <wouterabels@outlook.com>
2020-03-11 15:42:58 +0100

    added fading of sections

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+});
+
 var data = {
     labels: ['Java', 'Python', 'HTML(5)', 'CSS(3)', 'R',
         'SQL', 'JavaScript', 'Scrum(PSM 1)', 'MS Azure', 'Amazon Web Services',
